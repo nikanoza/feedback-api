f1f4557d142f5b720098bd3a667482593d51a573
Nika Nozadze <nikanozadze1010@gmail.com>
2024-03-10 23:29:05 +0400

    fix: change env variaable name

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connection = async () => {
   try {
-    const connectionUrl = process.env.MONGO_URl;
+    const connectionUrl = process.env.MONGO_URL;
     mongoose.connect(connectionUrl);
   } catch (error) {
     console.log("something went wrong");
--- src/controllers/category-controller.js
+++ src/controllers/category-controller.js
@@ -3,7 +3,6 @@ import Feedback from "../models/Feedback.js";
 export const getFeedbacks = async (req, res) => {
   try {
     const feedbacks = await Feedback.find();
-
     return res.status(200).json(feedbacks);
   } catch (error) {
     return res.status(501).json({ message: "server internal error" });
