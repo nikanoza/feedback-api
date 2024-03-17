a0f683e921bb30e5ab37292a042a1e1eab914811
Nika Nozadze <nikanozadze1010@gmail.com>
2024-03-17 22:22:38 +0400

    feat: add body-parser

--- src/server.js
+++ src/server.js
@@ -4,13 +4,16 @@ import dotenv from "dotenv";
 import connection from "./config/mongo.js";
 import feedbackRouter from "./routes/feedback-router.js";
 import authRouter from "./routes/auth-router.js";
+import bodyParser from "body-parser";
 
 dotenv.config();
 connection();
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(cors());
+
 app.use("/api", feedbackRouter);
 app.use("/api", authRouter);
 
